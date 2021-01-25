2a219140178fd156261c06f2a292614e8cb4e9a4
HawkBrave <doganyigityilmazz@gmail.com>
2021-01-25 17:37:56 +0300

    😇

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
+from imaplib import IMAP4_SSL
 from time import sleep
+import email
+import re
+
 
 class Bot:
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self, srs_info, mail_info):
+        self.srsuser, self.srspass = srs_info
+        self.mailuser, self.mailpass = mail_info
         self.driver = webdriver.Chrome()
+        self.mail_service = ''.join(re.search(r"(?:.*@)([\w\.]*)", self.mailuser).groups(1))
 
 
     def run(self):
@@ -15,6 +20,29 @@ class Bot:
         self.driver.get("https://stars.bilkent.edu.tr")
         self.driver.find_element_by_xpath("/html/body/div/div/div[1]/div/ul/li[3]/a").click()
         self.driver.find_element_by_xpath("/html/body/div/div[1]/div[2]/div[1]/div/section/form/fieldset/div/div[1]/div[1]/div/div/input")\
-                .send_keys(self.username + "\t" + self.password + "\n")
+                .send_keys(self.srsuser + "\t" + self.srspass + "\n")
 
         sleep(2)
+        if self.mail_service == "gmail.com":
+            self.mail_service = "imap." + self.mail_service
+        elif self.mail_service == "ug.bilkent.edu.tr":
+            raise Exception("Please use your gmail account. Bilkent mail is not supported")
+        else:
+            raise Exception("Invalid email address")
+
+        with IMAP4_SSL(self.mail_service) as m:
+            m.login(self.mailuser, self.mailpass)
+
+            m.select("Inbox")
+            inbox_item_list = m.search(None, '(FROM "starsmsg@bilkent.edu.tr")')[1][0].split()
+            sleep(1)
+            newest = inbox_item_list[-1]
+            raw_mail = m.fetch(newest, '(BODY[TEXT])')[1][0][1].decode('utf-8')
+
+        message = email.message_from_string(raw_mail)
+        code = re.search(r" \d{5}", str(message)).group(0).strip()
+
+        self.driver.find_element_by_xpath("/html/body/div/div[1]/div[2]/div[1]/div/section/form/fieldset/div/div[1]/div[1]/div/div/input")\
+                .send_keys(code + "\n")
+
+        sleep(60)
--- src/main.py
+++ src/main.py
@@ -4,16 +4,20 @@ import sys
 def main():
     try:
         with open("secret.txt", "r") as f:
-            username, password = f.readline().strip(), f.readline().strip()
-            b = Bot(username, password)
+            srs_info = [s.strip() for s in f.readline().split(" ")]
+            mail_info = [s.strip() for s in f.readline().split(" ")]
+
+        b = Bot(srs_info, mail_info)
+
+        b.run()
 
     except FileNotFoundError as e:
         print("Couldn't detect the necessary file: secret.txt")
-        print("If you haven't, please create a file and name it secret.txt. Enter your bilkent ID in the first line and password in the second.")
+        print("If you haven't, please create a file and name it secret.txt. Enter your bilkent ID and SRS password in the first line and your email username and password in the second.")
         sys.exit(1)
+    except Exception as e:
+        raise e
 
-
-    b.run()
 
 if __name__ == "__main__":
     main()
